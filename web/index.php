3e5a90b8f8cdbe2ac57c7e58cd5fca5b8545e372
dongww <dongww@gmail.com>
2014-01-28 16:12:12 +0800

    初始化项目

--- web/index.php
+++ web/index.php
@@ -1,9 +1,6 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Routing\Loader\YamlFileLoader;
-
 $app = new Core\Application();
 $app['debug'] = true;
 
