3feb47cba50b9640c81d4da9ed771c044e3f3f2e
dongweiwei <dongww@gmail.com>
2014-01-29 20:19:49 +0800

    增加了log支持

--- app/config/providers.php
+++ app/config/providers.php
@@ -15,6 +15,10 @@ $app->register(new \Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new \Silex\Provider\ValidatorServiceProvider());
 
 if ($app['debug']) {
+    $app->register(new \Silex\Provider\MonologServiceProvider(), array(
+        'monolog.logfile' => $app['cache_path'] . '/logs/debug.log',
+    ));
+
     $app->register(new \Silex\Provider\WebProfilerServiceProvider(), array(
         'profiler.cache_dir' => $app['cache_path'] . '/profiler',
         'profiler.mount_prefix' => $app['config.main']['debug_path'], // this is the default
