469ddfe2027a40c062c5bdbf6331a8598809f7e3
dongww <dongww@gmail.com>
2014-03-15 16:33:49 +0800

    修改providers文件

--- app/config/providers.php
+++ app/config/providers.php
@@ -1,4 +1,7 @@
 <?php
+/***********************
+ * Twig
+ **********************/
 $app->register(new \Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => $app['view_path'],
     'twig.options' => array(
@@ -8,19 +11,84 @@ $app->register(new \Silex\Provider\TwigServiceProvider(), array(
     )
 ));
 
+/***********************
+ * ServiceController
+ **********************/
 $app->register(new \Silex\Provider\ServiceControllerServiceProvider());
 
+/***********************
+ * Url 生成
+ **********************/
 $app->register(new \Silex\Provider\UrlGeneratorServiceProvider());
 
-$app->register(new \Silex\Provider\ValidatorServiceProvider());
+/***********************
+ * 验证
+ **********************/
+//$app->register(new \Silex\Provider\ValidatorServiceProvider());
 
+/***********************
+ * Session
+ **********************/
 $app->register(new Silex\Provider\SessionServiceProvider());
 
+/***********************
+ * Doctrine 数据库
+ **********************/
+//$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+//    'db.options' => array(
+//        'driver' => 'pdo_sqlite',
+//        'path' => $app['data_path'] . '/db/app.db',
+//    ),
+//));
+
+/***********************
+ * 表单
+ **********************/
+//$app->register(new Silex\Provider\FormServiceProvider());
+
+/***********************
+ * http 缓存
+ **********************/
+//$app->register(new Silex\Provider\HttpCacheServiceProvider(), array(
+//    'http_cache.cache_dir' => $app['cache_path'] . '/http',
+//));
+
+/***********************
+ * HttpFragment
+ **********************/
+//$app->register(new Silex\Provider\HttpFragmentServiceProvider());
+
+/***********************
+ * 序列化
+ **********************/
+//$app->register(new Silex\Provider\SerializerServiceProvider());
+
+/***********************
+ * 邮件
+ **********************/
+//$app['swiftmailer.options'] = array(
+//    'host' => $app['config.main']['mail_host'],
+//    'port' => $app['config.main']['mail_port'],
+//    'username' => $app['config.main']['mail_username'],
+//    'password' => $app['config.main']['mail_password'],
+//    'encryption' => $app['config.main']['mail_encryption'],
+//    'auth_mode' => $app['config.main']['mail_auth_mode']
+//);
+
+/***********************
+ * Debug
+ **********************/
 if ($app['debug']) {
+    /***********************
+     * 日志
+     **********************/
     $app->register(new \Silex\Provider\MonologServiceProvider(), array(
         'monolog.logfile' => $app['cache_path'] . '/logs/debug.log',
     ));
 
+    /***********************
+     * Debug 信息条
+     **********************/
     $app->register(new \Silex\Provider\WebProfilerServiceProvider(), array(
         'profiler.cache_dir' => $app['cache_path'] . '/profiler',
         'profiler.mount_prefix' => $app['config.main']['debug_path'], // this is the default
