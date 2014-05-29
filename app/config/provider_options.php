0c12e4c8059146350c28eef0072e4fddb9aae5f4
dongww <dongww@gmail.com>
2014-05-29 11:31:57 +0800

    确认最低PHP版本，要求5.4

--- app/config/provider_options.php
+++ app/config/provider_options.php
@@ -18,11 +18,12 @@ if ($config['mail']) {
  **********************/
 if ($config['doctrine']) {
     $app['db.options'] = array(
-        'driver'   => 'pdo_sqlite',
-        'path'     => $app['data_path'] . '/db/app.db',
-        'dbname'   => '',
-        'host'     => '',
-        'user'     => '',
+        'driver'   => 'pdo_mysql',
+//        'driver'   => 'pdo_sqlite',
+//        'path'     => $app['data_path'] . '/db/app.db',
+        'dbname'   => 'dbal_wrap',
+        'host'     => 'localhost',
+        'user'     => 'root',
         'password' => '',
         'charset'  => 'utf8',
         'port'     => '',
@@ -35,12 +36,12 @@ if ($config['doctrine']) {
 
 $app['security.firewalls'] = array(
     'admin'     => array(
-        'pattern' => '^/admin',
-        'form'    => array(
+        'pattern'     => '^/admin',
+        'form'        => array(
             'login_path' => '/login',
             'check_path' => '/admin/login_check'
         ),
-        'logout'  => array(
+        'logout'      => array(
             'logout_path' => '/admin/logout'
         ),
 //        'http' => true,
@@ -49,7 +50,7 @@ $app['security.firewalls'] = array(
             'always_remember_me' => false,
             /* Other options */
         ),
-        'users'   => array(
+        'users'       => array(
             // 密码为 foo
             'admin' => array(
                 'ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='
